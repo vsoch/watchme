a9f7271e56b01c5c14efb035fcf6924c7a50870b
Vanessa Sochat <vsochat@stanford.edu>
2019-06-06 18:09:27 -0400

    updating long description type for pypi
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- setup.py
+++ setup.py
@@ -99,6 +99,7 @@ if __name__ == "__main__":
           license=LICENSE,
           description=DESCRIPTION,
           long_description=LONG_DESCRIPTION,
+          long_description_content_type="text/markdown",
           keywords=KEYWORDS,
           setup_requires=["pytest-runner"],
           tests_require=["pytest"],
