ca401a5901b73aea3f2afc6aada86041412b4c0a
vsoch <vsochat@stanford.edu>
2020-04-10 12:12:48 -0600

    running black on setup.py
    Signed-off-by: vsoch <vsochat@stanford.edu>

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Copyright (C) 2019-2020 Vanessa Sochat.
 
@@ -6,7 +6,7 @@ This Source Code Form is subject to the terms of the
 Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
 with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-'''
+"""
 
 
 from setuptools import setup, find_packages
@@ -18,12 +18,12 @@ import os
 
 
 def get_lookup():
-    '''get version by way of singularity.version, returns a
+    """get version by way of singularity.version, returns a
     lookup dictionary with several global variables without
     needing to import singularity
-    '''
+    """
     lookup = dict()
-    version_file = os.path.join('watchme', 'version.py')
+    version_file = os.path.join("watchme", "version.py")
     with open(version_file) as filey:
         exec(filey.read(), lookup)
     return lookup
@@ -31,8 +31,8 @@ def get_lookup():
 
 # Read in requirements
 def get_requirements(lookup=None, key="INSTALL_REQUIRES"):
-    '''get_requirements reads in requirements and versions from
-    the lookup obtained with get_lookup'''
+    """get_requirements reads in requirements and versions from
+    the lookup obtained with get_lookup"""
 
     if lookup is None:
         lookup = get_lookup()
@@ -42,9 +42,9 @@ def get_requirements(lookup=None, key="INSTALL_REQUIRES"):
         module_name = module[0]
         module_meta = module[1]
         if "exact_version" in module_meta:
-            dependency = "%s==%s" % (module_name, module_meta['exact_version'])
+            dependency = "%s==%s" % (module_name, module_meta["exact_version"])
         elif "min_version" in module_meta:
-            min_version = module_meta['min_version']
+            min_version = module_meta["min_version"]
             if min_version is None:
                 dependency = module_name
             else:
@@ -59,15 +59,15 @@ os.chdir(install_path)
 
 # Get version information from the lookup
 lookup = get_lookup()
-VERSION = lookup['__version__']
-NAME = lookup['NAME']
-AUTHOR = lookup['AUTHOR']
-AUTHOR_EMAIL = lookup['AUTHOR_EMAIL']
-PACKAGE_URL = lookup['PACKAGE_URL']
-KEYWORDS = lookup['KEYWORDS']
-DESCRIPTION = lookup['DESCRIPTION']
-LICENSE = lookup['LICENSE']
-with open('README.md') as filey:
+VERSION = lookup["__version__"]
+NAME = lookup["NAME"]
+AUTHOR = lookup["AUTHOR"]
+AUTHOR_EMAIL = lookup["AUTHOR_EMAIL"]
+PACKAGE_URL = lookup["PACKAGE_URL"]
+KEYWORDS = lookup["KEYWORDS"]
+DESCRIPTION = lookup["DESCRIPTION"]
+LICENSE = lookup["LICENSE"]
+with open("README.md") as filey:
     LONG_DESCRIPTION = filey.read()
 
 ################################################################################
@@ -79,46 +79,47 @@ if __name__ == "__main__":
 
     # Install all exporters and/or watchers
     INSTALL_REQUIRES = get_requirements(lookup)
-    INSTALL_ALL = get_requirements(lookup, 'INSTALL_ALL')
-    WATCHERS = get_requirements(lookup, 'INSTALL_WATCHERS')
+    INSTALL_ALL = get_requirements(lookup, "INSTALL_ALL")
+    WATCHERS = get_requirements(lookup, "INSTALL_WATCHERS")
 
     # Watchers
-    URLS_DYNAMIC = get_requirements(lookup, 'INSTALL_URLS_DYNAMIC')
-    PSUTILS = get_requirements(lookup, 'INSTALL_PSUTILS')
+    URLS_DYNAMIC = get_requirements(lookup, "INSTALL_URLS_DYNAMIC")
+    PSUTILS = get_requirements(lookup, "INSTALL_PSUTILS")
 
-    setup(name=NAME,
-          version=VERSION,
-          author=AUTHOR,
-          author_email=AUTHOR_EMAIL,
-          maintainer=AUTHOR,
-          maintainer_email=AUTHOR_EMAIL,
-          packages=find_packages(),
-          include_package_data=True,
-          zip_safe=False,
-          url=PACKAGE_URL,
-          license=LICENSE,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          long_description_content_type="text/markdown",
-          keywords=KEYWORDS,
-          setup_requires=["pytest-runner"],
-          tests_require=["pytest"],
-          install_requires=INSTALL_REQUIRES,
-          extras_require={
-              'all': [INSTALL_ALL],
-              'watchers': [WATCHERS],
-              'watcher-urls-dynamic': [URLS_DYNAMIC],
-              'watcher-psutils': [PSUTILS]
-          },
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'Intended Audience :: Developers',
-              'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Unix',
-              'Programming Language :: Python :: 2.7',
-              'Programming Language :: Python :: 3',
-          ],
-
-          entry_points={'console_scripts': ['watchme=watchme.client:main']})
+    setup(
+        name=NAME,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        maintainer=AUTHOR,
+        maintainer_email=AUTHOR_EMAIL,
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        url=PACKAGE_URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        keywords=KEYWORDS,
+        setup_requires=["pytest-runner"],
+        tests_require=["pytest"],
+        install_requires=INSTALL_REQUIRES,
+        extras_require={
+            "all": [INSTALL_ALL],
+            "watchers": [WATCHERS],
+            "watcher-urls-dynamic": [URLS_DYNAMIC],
+            "watcher-psutils": [PSUTILS],
+        },
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Scientific/Engineering",
+            "Operating System :: Unix",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+        ],
+        entry_points={"console_scripts": ["watchme=watchme.client:main"]},
+    )
